14036f859f61ad5b66ffe92e166de8c4b2362745
Feng Changtao <70519041+fengChangTao@users.noreply.github.com>
2025-06-18 15:15:03 +0800

    Update script_open_forum_new_tab.js

--- script_open_forum_new_tab.js
+++ script_open_forum_new_tab.js
@@ -14,6 +14,8 @@
 // @match        http://bbs.pcbeta.com/*
 // @run-at       document-start
 // @grant        none
+// @downloadURL  https://github.com/fengChangTao/fengChangTao.github.io/raw/refs/heads/main/script_open_forum_new_tab.js
+// @updateURL https://github.com/fengChangTao/fengChangTao.github.io/raw/refs/heads/main/script_open_forum_new_tab.js
 // ==/UserScript==
 
 (function() {
